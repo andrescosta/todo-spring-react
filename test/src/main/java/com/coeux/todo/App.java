129da415a2c260c69f87dd35c8a8ae611940025f
Andres <andrescosta@users.noreply.github.com>
2023-03-14 12:04:47 -0400

    Testcontainer (#1)
    * integration tests

---
+++ test/src/main/java/com/coeux/todo/App.java
@@ -0,0 +1,13 @@
+package com.coeux.todo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Run tests!" );
+    }
+}
---
+++ test/test.cmd
@@ -0,0 +1,1 @@
+.\mvnw surefire:test
