7ea5809fb3d7d9dc37fca798c34125dc133e364e
Andres <andres.costa@gmail.com>
2024-03-07 10:41:39 -0500

    k8s

---
+++ manifests/cluster.ps1
@@ -0,0 +1,3 @@
+kind create cluster --config .\cluster.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller  --timeout=90s
